1ccd1097f10f15c60b582fef605f05882b7c02f1
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2018-07-05 21:18:46 +0430

    Add custom menu

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-import {app , BrowserWindow} from 'electron';
+import {app , BrowserWindow,Menu,MenuItem} from 'electron';
 import url from 'url';
 import path from 'path';
 import electronReload from 'electron-reload';
 electronReload(__dirname);
 import Devtron from 'devtron';
 
+
+let menu = new Menu();
+let menuitem1 = new MenuItem({
+    label : 'File1',
+    submenu : [
+        {
+            label : 'Exit',
+            click(){
+                app.quit();
+            }
+        }
+    ]
+});
+
+menu.append(menuitem1)  ;
 
 app.on('ready', () => {
     let mainWin = new BrowserWindow({
@@ -17,4 +32,7 @@ app.on('ready', () => {
         //backgroundColor : '#27ae60'
     });
     mainWin.loadURL(`file://${__dirname}/index.html`);
+
+
+    Menu.setApplicationMenu(menu);
 });
